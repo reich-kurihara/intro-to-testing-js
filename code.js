49d39ea79a63f89bb55f642f0749e7f7e55ada89
Sophia <sophia.sudiacal@gmail.com>
2020-10-01 14:18:59 -0500

    wrote my first test! sayHello function

--- code.js
+++ code.js
@@ -1,4 +1,14 @@
-// helloWorld function 
+// helloWorld function
 function helloWorld() {
     return "Hello, World!";
+}
+
+// const helloWorld = function(){
+//     return "Hello, World!";
+// };
+
+// sayHello function
+
+function sayHello(){
+
 }
